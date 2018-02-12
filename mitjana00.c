94500bad978cb850446c6a74afcb2eea9494a116
damoma1718daw2 <damoma1718daw2@gmail.com>
2018-02-12 19:47:38 +0100

    Conflicte 1 entre master i br0

--- mitjana00.c
+++ mitjana00.c
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 
+
 int main() {
 	int i=0;
 	float suma=0.0;
@@ -22,5 +23,6 @@ int main() {
 	}
 	mitjana=suma/numero_repeticions;
 	printf("La mitjana resultant és: %.2f\n",mitjana);	
+	printf("La mitjana ha estat trobada amb èxit\n");
 	return 0;
 }
